5f7d21a8741d4a71eba18b0395e1c906ce82bf82
LuDong <noodle1983@126.com>
2019-12-25 18:44:57 +0800

    llx is too long

--- log.h
+++ log.h
@@ -6,24 +6,24 @@
 #if 1
 #include <android/log.h>
 #define MY_VERBOSE(...)
-#define MY_LOG(fmt,...)  __android_log_print(ANDROID_LOG_DEBUG  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_METHOD(fmt,...)  __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_INFO(fmt, ...) __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_ERROR(fmt,...) __android_log_print(ANDROID_LOG_ERROR  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
+#define MY_LOG(fmt,...)  __android_log_print(ANDROID_LOG_DEBUG  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_METHOD(fmt,...)  __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_INFO(fmt, ...) __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_ERROR(fmt,...) __android_log_print(ANDROID_LOG_ERROR  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
 
 #elif 0
 #define MY_VERBOSE(fmt, ...)
-#define MY_LOG(fmt, ...) printf( "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_METHOD(fmt, ...) printf( "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_ERROR(fmt, ...) printf( "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
+#define MY_LOG(fmt, ...) printf( "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_METHOD(fmt, ...) printf( "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_ERROR(fmt, ...) printf( "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
 
 #elif __ANDROID__
 #include <android/log.h>
 #define MY_VERBOSE(fmt, ...)
 #define MY_LOG(fmt, ...) 
 #define MY_METHOD(fmt, ...)
-#define MY_INFO(fmt, ...) __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
-#define MY_ERROR(fmt, ...) __android_log_print(ANDROID_LOG_ERROR  , "il2cpp", "[%08llx]" fmt "\n", (unsigned long long)pthread_self(), ##__VA_ARGS__)
+#define MY_INFO(fmt, ...) __android_log_print(ANDROID_LOG_INFO  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
+#define MY_ERROR(fmt, ...) __android_log_print(ANDROID_LOG_ERROR  , "il2cpp", "[%zx]" fmt "\n", (size_t)pthread_self(), ##__VA_ARGS__)
 #endif
 
 #define ALOGD MY_LOG
